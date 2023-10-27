d880079154bcc7a4a01837003f58638d7afaed49
jaimano T <jaimanotj@gmail.com>
2023-10-27 23:53:23 +0530

    created signup api route

--- server/index.js
+++ server/index.js
@@ -1,18 +1,23 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import authRouter from "./routes/auth.route.js";
+
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log("Connected to Database!");
+    console.log("Connected to Database.");
   })
   .catch((err) => {
     console.log(err);
   });
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
-  console.log("Server running in port 3000!!");
+  console.log("Server running in port 3000.");
 });
+
+app.use("/api/auth", authRouter);
